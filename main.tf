5303ef1e584c998a10306f679c27f388de8e40dd
Thsx <50934142+Thsx@users.noreply.github.com>
2019-07-01 14:03:33 -0300

    Update main.tf
    Codigo modificado adicionado subnet e security group

--- main.tf
+++ main.tf
@@ -5,3 +5,18 @@ resource "aws_instance" "webzada" {
   ami = "${var.ami}"
   instance_type = "${var.type}"
   }
+data "aws_subnet" "selected" {
+  id = "${var.subnet_id}"
+}
+resource "aws_security_group" "subnet" {
+  vpc_id = "${data.aws_subnet.selected.vpc_id}"
+
+  ingress {
+    cidr_blocks = ["${data.aws_subnet.selected.cidr_block}"]
+    from_port   = "${var.i_from_port}"
+    to_port     = "${var.i_to_port}"
+    protocol    = "${var.i_protocol}"
+  }
+  
+}
+
